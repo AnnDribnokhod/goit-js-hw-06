0de593645847e0924e247ea9ff5f84771172a069
AnnDribnokhod <88674960+AnnDribnokhod@users.noreply.github.com>
2023-07-06 00:32:25 +0300

    Delete src directory

--- src/js/task-07.js
+++
@@ -1,9 +0,0 @@
-const getInputRange = document.querySelector("#font-size-control");
-const getTextRange = document.querySelector("#text");
-
-getInputRange.addEventListener("input", handleInputRange);
-
-function handleInputRange(evt) {
-  getTextRange.style.fontSize = evt.currentTarget.value + "px";
-  console.dir(evt.currentTarget.value + "px");
-}
